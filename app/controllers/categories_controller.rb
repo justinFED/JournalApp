5585549984d3bd47b65a998135d137a3ae2197d3
Justin Cantillo <justin.tech96@gmail.com>
2023-12-07 00:07:59 +0800

    Delete and Edit button for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,26 +1,48 @@
 class CategoriesController < ApplicationController
-    def index
-      @categories = Category.all
-    end
-  
-    def new
-      @category = Category.new
-    end
-  
-    def create
-      @category = Category.new(category_params)
-  
-      if @category.save
-        redirect_to categories_path, notice: 'Category was successfully created.'
-      else
-        render :new
-      end
-    end
-  
-    private
-  
-    def category_params
-      params.require(:category).permit(:name)  # Adjust permitted parameters based on your Category model attributes
+  before_action :set_category, only: [:edit, :update, :destroy]
+
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to categories_path, notice: 'Category was successfully created.'
+    else
+      render :new
     end
   end
-  
+
+  def edit
+    # The set_category before_action ensures that @category is set
+  end
+
+  def update
+    if @category.update(category_params)
+      redirect_to categories_path, notice: 'Category was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @category.destroy
+    redirect_to categories_path, notice: 'Category was successfully destroyed.'
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+end
